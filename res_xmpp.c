ee56e66bac3f27374f4290dee2b16612e29aec51
Etienne Lessard <elessard@proformatique.com>
2016-10-12 09:19:17 -0400

    update res_xmpp.c to Asterisk 14.0.2
    Not stricly compatible with Asterisk 13 but I'll update the patch to
    make it compatible for the two versions at the same time.

--- res_xmpp.c
+++ res_xmpp.c
@@ -48,7 +48,7 @@
 
 #include "asterisk.h"
 
-ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
+ASTERISK_REGISTER_FILE()
 
 #include <ctype.h>
 #include <iksemel.h>
@@ -4665,9 +4665,9 @@ static int reload(void)
 }
 
 AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_GLOBAL_SYMBOLS | AST_MODFLAG_LOAD_ORDER, "Asterisk XMPP Interface",
-		.support_level = AST_MODULE_SUPPORT_CORE,
-		.load = load_module,
-		.unload = unload_module,
-		.reload = reload,
-		.load_pri = AST_MODPRI_CHANNEL_DEPEND,
-	       );
+	.support_level = AST_MODULE_SUPPORT_CORE,
+	.load = load_module,
+	.unload = unload_module,
+	.reload = reload,
+	.load_pri = AST_MODPRI_CHANNEL_DEPEND,
+);
